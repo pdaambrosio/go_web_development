d0b482e4c88cde4c6d3c5b21a3c126e52231f230
pdaambrosio <pda.ambrosio@gmail.com>
2023-09-02 15:20:29 -0300

    fix: css file

--- lab1/main.go
+++ lab1/main.go
@@ -8,8 +8,12 @@ import (
 	"strings"
 )
 
+// The StaticHandler function serves static files such as HTML and CSS files.
+// It takes a http.ResponseWriter and a http.Request as arguments.
+// It opens the requested file and copies it to the http.ResponseWriter.
+// If the file does not exist, it returns a 404 Not Found error.
 func StaticHandler(w http.ResponseWriter, r *http.Request) {
-	page, err := os.Open("static" + r.URL.Path + ".html")
+	page, err := os.Open("static" + r.URL.Path)
 
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
@@ -25,6 +29,7 @@ func StaticHandler(w http.ResponseWriter, r *http.Request) {
 	io.Copy(w, page)
 }
 
+// The main function sets up a static file server and listens for incoming HTTP requests on port 8080.
 func main() {
 	http.HandleFunc("/", StaticHandler)
 	http.ListenAndServe(":8080", nil)
